eee703f21c801dd3b80ebf0bda28ec1bde8b91d2
Bahadır Gunuvar <bahadir.223@hotmail.com>
2023-04-04 04:33:32 +0300

    FilmTahmin Project

---
+++ src/main/java/FilmTahmin/FilmTahmin/Runner.java
@@ -0,0 +1,14 @@
+package FilmTahmin.FilmTahmin;
+
+import static practices.Projeler.FilmTahmin.FilmTahmin.Methods.baslatMethod;
+
+public class Runner extends Depo {
+
+
+    public static void main(String[] args) {
+
+        baslatMethod();
+    }
+
+
+}
